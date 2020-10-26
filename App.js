df7dfa908f531dbd0ff5bcaa94d869ca827f0d33
mickael-h <mickael.hassine@gmail.com>
2020-10-26 20:12:15 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
